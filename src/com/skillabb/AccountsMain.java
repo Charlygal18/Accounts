470a6b6d3c8f9e1138fee5f23f660e66e181cac5
Carlos Eduardo Galván Salinas <carlosgalvans1999@gmail.com>
2024-01-29 17:50:36 -0600

    Third requirement: creation of 3 accounts and foreach to them

--- src/com/skillabb/AccountsMain.java
+++ src/com/skillabb/AccountsMain.java
@@ -6,14 +6,27 @@ import com.skillabb.service.imp.AccountsImp;
 public class AccountsMain {
 
 	public static void main(String[] args) {
+		//Primer requerimiento
 		Account account = new Account("Carlos Galván", "1", 1.0);
 		
 		System.out.println("Nombre del cliente: " + account.customerName);
 		System.out.println("Número de cuenta: " + account.accountNumber);
 		System.out.println("Balance: " + account.balance);
 		
+		//Segundo requerimiento
 		AccountsImp accountsImp = new AccountsImp();
 		accountsImp.validateAccounts(account);
+		
+		//Terceer requerimiento
+		System.out.println("--------------------------------");
+		accountsImp.generateAccounts();
+		
+		for(Account accounts:accountsImp.getAccounts()) {
+			System.out.println("Nombre del cliente: " + accounts.customerName);
+			System.out.println("Número de cuenta: " + accounts.accountNumber);
+			System.out.println("Balance: " + accounts.balance);
+			System.out.println("--------------------------------");
+		}
 		
 	}
 
--- src/com/skillabb/service/imp/AccountsImp.java
+++ src/com/skillabb/service/imp/AccountsImp.java
@@ -4,6 +4,7 @@ import com.skillabb.model.Account;
 
 public class AccountsImp {
 	private static final String NUM_CUENTA_NO_VALIDO = "32145678";
+	private Account[] accounts = new Account[3];
 	
 	public void validateAccounts(Account account) {
 		if(account.balance > 0.0 && !account.accountNumber.equals(NUM_CUENTA_NO_VALIDO)) {
@@ -12,6 +13,21 @@ public class AccountsImp {
 		else {
 			System.out.println("La cuenta no existe");
 		}
+	}
+	
+	public void generateAccounts() {
+		accounts[0] = new Account("Alan Saji", "2", 200.0);
+		accounts[1] = new Account("Diana Escorcia", "3", 230.5);
+		accounts[2] = new Account("Eloy Gómez", "4", 190.2);
+		
+	}
+
+	public Account[] getAccounts() {
+		return accounts;
+	}
+
+	public void setAccounts(Account[] accounts) {
+		this.accounts = accounts;
 	}
 
 }
